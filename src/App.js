fd37db5644bd66034349aac953301595a696d980
Eduardo <drdpedroso@gmail.com>
2020-03-25 17:35:04 -0300

    Add tests and remove file content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css'
-import useAxios from 'axios-hooks'
-function App() {
-  const [{ data }] = useAxios(
-      'http://localhost:3001/images'
-  )
 
+function App() {
   return (
     <div>
-      {
-        data && data.map(i => {
-          return <img src={`http://localhost:3001${i.path}`} />
-        })
-      }
+
     </div>
   );
 }
