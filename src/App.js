7ddfe8b0805bf5d706ffb5e035a9312dc61be1b0
Eduardo Pedroso <drdpedroso@gmail.com>
2020-03-25 17:37:58 -0300

    Gabarito (#1)
    * safe
    
    * Add tests and remove file content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css'
-import useAxios from 'axios-hooks'
-function App() {
-  const [{ data, loading }] = useAxios(
-      'http://localhost:3001/images'
-  )
 
+function App() {
   return (
     <div>
-      {
-        data && data.map(i => {
-          return <img src={`http://localhost:3001${i.path}`} />
-        })
-      }
+
     </div>
   );
 }
